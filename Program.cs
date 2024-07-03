baeb2f4bca18e04dd1a038bf94b7c031f9844387
se77ings <se77ings@gmail.com>
2024-07-02 21:53:25 -0300

    initial part

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Financas.view;
+
 namespace Financas
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Financas
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new EntradasSaidas());
         }
     }
 }
---
+++ view/EntradasSaidas/EntradasSaidas.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Financas.view
+{
+    partial class EntradasSaidas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "dd/MM/YY ";
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(24, 48);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(117, 23);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Data do Lançamento";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(243, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Categoria";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(458, 258);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Lançamentos";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(239, 174);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 23);
+            button1.TabIndex = 9;
+            button1.Text = "Registrar Lançamento";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(76, 175);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(120, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 178);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Valor (R$) : ";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Location = new Point(18, 88);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(178, 72);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Tipo da Transação";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Checked = true;
+            radioButton2.Location = new Point(100, 31);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(58, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Saídas";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 31);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(70, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.Text = "Entradas";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(239, 119);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(169, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(239, 98);
+            label3.Name = "label3";
+            label3.Size = new Size(144, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Descrição do Lançamento";
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = SystemColors.Control;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FlatStyle = FlatStyle.System;
+            comboBox1.ForeColor = Color.Black;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Escolha" });
+            comboBox1.Location = new Point(243, 48);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(169, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // EntradasSaidas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(490, 326);
+            Controls.Add(groupBox1);
+            Name = "EntradasSaidas";
+            Text = "EntradasSaidas";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Label label2;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private ComboBox comboBox1;
+        private GroupBox groupBox2;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label4;
+    }
+}
