3b887a6ac38e4da08743076eac1cb9e1128e9997
jonathanrbernardes <127804571+jonathanrbernardes@users.noreply.github.com>
2024-07-04 00:49:02 -0300

    Add files via upload

---
+++ PaginaPrincipal.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Financas.view
+{
+    partial class PaginaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.Control;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(30, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(720, 312);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Carteira";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ControlDark;
+            label2.Location = new Point(16, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Saldo do dia";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(152, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 5;
+            textBox1.Text = "R$";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Arial Narrow", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(404, 56);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(119, 28);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(94, 111);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 7;
+            button1.Text = "Extrato";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // PaginaPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "PaginaPrincipal";
+            Text = "PaginaPrincipal";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBox1;
+        private Label label2;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+    }
+}
