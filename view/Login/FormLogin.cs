21f10878aab9ad554f15b8c51e570ab634f4777a
Cesar Santana <108631323+cesar1014@users.noreply.github.com>
2024-07-16 21:44:18 -0300

    Add files via upload

---
+++ view/Login/FormLogin.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class FormLogin : Form
+    {
+        private static string senha;
+        private static string usuario;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+            SetPlaceholderText(txtSenha, "Senha");
+
+            // Verifique se já existe um usuário registrado
+            if (string.IsNullOrEmpty(senha))
+            {
+                FormRegister formRegister = new FormRegister();
+                formRegister.ShowDialog();
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtSenha = new TextBox();
+            this.btnLogin = new Button();
+            this.lblErro = new Label();
+            this.SuspendLayout();
+
+            // txtSenha
+            this.txtSenha.Location = new System.Drawing.Point(12, 12);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(260, 20);
+            this.txtSenha.TabIndex = 0;
+            this.txtSenha.Enter += new EventHandler(this.RemoverTextoPlaceholder);
+            this.txtSenha.Leave += new EventHandler(this.DefinirTextoPlaceholder);
+
+            // btnLogin
+            this.btnLogin.Location = new System.Drawing.Point(12, 38);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(260, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new EventHandler(this.btnLogin_Click);
+
+            // lblErro
+            this.lblErro.AutoSize = true;
+            this.lblErro.ForeColor = System.Drawing.Color.Red;
+            this.lblErro.Location = new System.Drawing.Point(12, 64);
+            this.lblErro.Name = "lblErro";
+            this.lblErro.Size = new System.Drawing.Size(0, 13);
+            this.lblErro.TabIndex = 2;
+
+            // FormLogin
+            this.ClientSize = new System.Drawing.Size(284, 91);
+            this.Controls.Add(this.lblErro);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private TextBox txtSenha;
+        private Button btnLogin;
+        private Label lblErro;
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string senhaDigitada = txtSenha.Text;
+            string hashedSenhaDigitada = ComputeSha256Hash(senhaDigitada);
+
+            if (hashedSenhaDigitada == senha)
+            {
+                FormMain formMain = new FormMain(senha, usuario);
+                formMain.Show();
+                this.Hide();
+            }
+            else
+            {
+                lblErro.Text = "Senha incorreta!";
+            }
+        }
+
+        private static string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static void Setsenha(string novaSenha)
+        {
+            senha = novaSenha;
+        }
+
+        public static void Setusuario(string novoUsuario)
+        {
+            usuario = novoUsuario;
+        }
+
+        private void RemoverTextoPlaceholder(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Senha")
+            {
+                textBox.Text = "";
+                textBox.ForeColor = System.Drawing.Color.Black;
+                textBox.PasswordChar = '*';
+            }
+        }
+
+        private void DefinirTextoPlaceholder(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.ForeColor = System.Drawing.Color.Gray;
+                textBox.PasswordChar = '\0';
+                textBox.Text = "Senha";
+            }
+        }
+
+        private void SetPlaceholderText(TextBox textBox, string placeholderText)
+        {
+            textBox.ForeColor = System.Drawing.Color.Gray;
+            textBox.PasswordChar = '\0';
+            textBox.Text = placeholderText;
+        }
+    }
+}
---
+++ view/Login/FormMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class FormMain : Form
+    {
+        private string senha;
+        private string usuario;
+
+        public FormMain(string senha, string usuario)
+        {
+            InitializeComponent();
+            this.senha = senha;
+            this.usuario = usuario;
+            lblBemVindo.Text = "Bem-vindo " + usuario;
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblBemVindo = new Label();
+            this.btnAlterarSenha = new Button();
+            this.SuspendLayout();
+
+            // lblBemVindo
+            this.lblBemVindo.AutoSize = true;
+            this.lblBemVindo.Location = new System.Drawing.Point(12, 9);
+            this.lblBemVindo.Name = "lblBemVindo";
+            this.lblBemVindo.Size = new System.Drawing.Size(64, 13);
+            this.lblBemVindo.TabIndex = 0;
+
+            // btnAlterarSenha
+            this.btnAlterarSenha.Location = new System.Drawing.Point(12, 35);
+            this.btnAlterarSenha.Name = "btnAlterarSenha";
+            this.btnAlterarSenha.Size = new System.Drawing.Size(260, 23);
+            this.btnAlterarSenha.TabIndex = 1;
+            this.btnAlterarSenha.Text = "Alterar Senha";
+            this.btnAlterarSenha.UseVisualStyleBackColor = true;
+            this.btnAlterarSenha.Click += new EventHandler(this.btnAlterarSenha_Click);
+
+            // FormMain
+            this.ClientSize = new System.Drawing.Size(284, 81);
+            this.Controls.Add(this.btnAlterarSenha);
+            this.Controls.Add(this.lblBemVindo);
+            this.Name = "FormMain";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private Label lblBemVindo;
+        private Button btnAlterarSenha;
+
+        private void btnAlterarSenha_Click(object sender, EventArgs e)
+        {
+            FormChangePassword formChangePassword = new FormChangePassword(senha);
+            formChangePassword.ShowDialog();
+        }
+    }
+}
