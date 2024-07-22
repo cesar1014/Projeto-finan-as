1e6433213151d829ad0f956d63aaac4dcc283536
Cesar Santana <108631323+cesar1014@users.noreply.github.com>
2024-07-22 08:22:37 -0300

    Comentei o Program.cs

--- Program.cs
+++ Program.cs
@@ -12,22 +12,28 @@ namespace Financas
         [STAThread]
         static void Main()
         {
+            // Inicializa a configuração da aplicação (visual, etc.)
             ApplicationConfiguration.Initialize();
 
             using (var context = new DataContext())
             {
+                //Aplica as migrações pendentes ao banco de dados
                 context.Database.Migrate();
 
+                // Verifica se existe algum usuário registrado no banco de dados
                 bool hasUsers = context.Usuario.Any();
                 if (hasUsers)
                 {
+                    // Se existirem usuários, abre o formulário de login
                     Application.Run(new FormLogin());
                 }
                 else
                 {
+                    // Se não existirem usuários, abre o formulário de registro
                     var registerForm = new FormRegister();
                     Application.Run(registerForm);
 
+                    // Se o registro foi bem-sucedido, abre o formulário de login
                     if (registerForm.RegistroBemSucedido)
                     {
                         Application.Run(new FormLogin());
