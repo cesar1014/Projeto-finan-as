eba05167a0c7cd1d292e38a924a984e451fece81
Cesar Santana <108631323+cesar1014@users.noreply.github.com>
2024-07-22 08:01:35 -0300

    Update FormTrocaSenha.cs

--- view/Login/FormTrocaSenha.cs
+++ view/Login/FormTrocaSenha.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows.Forms;
@@ -10,6 +10,9 @@ namespace LoginApp
     public partial class FormChangePassword : Form
     {
         private string nomeUsuario;
+        private Label label3;
+        private Label label1;
+        private Label label2;
         private readonly UsuarioController usuarioController;
 
         public FormChangePassword(string nomeUsuario, UsuarioController usuarioController)
@@ -136,72 +139,127 @@ namespace LoginApp
 
         private void InitializeComponent()
         {
-            this.txtSenhaAtual = new TextBox();
-            this.txtNovaSenha = new TextBox();
-            this.txtConfirmarNovaSenha = new TextBox();
-            this.btnAlterarSenha = new Button();
-            this.lblErro = new Label();
-            this.SuspendLayout();
-
+            txtSenhaAtual = new TextBox();
+            txtNovaSenha = new TextBox();
+            txtConfirmarNovaSenha = new TextBox();
+            btnAlterarSenha = new Button();
+            lblErro = new Label();
+            lblTitulo = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
             // txtSenhaAtual
-            this.txtSenhaAtual.Font = new Font("Segoe UI", 10F);
-            this.txtSenhaAtual.Location = new System.Drawing.Point(12, 30);
-            this.txtSenhaAtual.Name = "txtSenhaAtual";
-            this.txtSenhaAtual.Size = new System.Drawing.Size(260, 25);
-            this.txtSenhaAtual.TabIndex = 0;
-            this.txtSenhaAtual.Enter += new EventHandler(this.RemoverTextoPlaceholder);
-            this.txtSenhaAtual.Leave += new EventHandler(this.DefinirTextoPlaceholder);
-
+            // 
+            txtSenhaAtual.Font = new Font("Segoe UI", 10F);
+            txtSenhaAtual.Location = new Point(60, 150);
+            txtSenhaAtual.Name = "txtSenhaAtual";
+            txtSenhaAtual.Size = new Size(350, 25);
+            txtSenhaAtual.TabIndex = 0;
+            txtSenhaAtual.Enter += RemoverTextoPlaceholder;
+            txtSenhaAtual.Leave += DefinirTextoPlaceholder;
+            // 
             // txtNovaSenha
-            this.txtNovaSenha.Font = new Font("Segoe UI", 10F);
-            this.txtNovaSenha.Location = new System.Drawing.Point(12, 70);
-            this.txtNovaSenha.Name = "txtNovaSenha";
-            this.txtNovaSenha.Size = new System.Drawing.Size(260, 25);
-            this.txtNovaSenha.TabIndex = 1;
-            this.txtNovaSenha.Enter += new EventHandler(this.RemoverTextoPlaceholder);
-            this.txtNovaSenha.Leave += new EventHandler(this.DefinirTextoPlaceholder);
-
+            // 
+            txtNovaSenha.Font = new Font("Segoe UI", 10F);
+            txtNovaSenha.Location = new Point(60, 200);
+            txtNovaSenha.Name = "txtNovaSenha";
+            txtNovaSenha.Size = new Size(350, 25);
+            txtNovaSenha.TabIndex = 1;
+            txtNovaSenha.Enter += RemoverTextoPlaceholder;
+            txtNovaSenha.Leave += DefinirTextoPlaceholder;
+            // 
             // txtConfirmarNovaSenha
-            this.txtConfirmarNovaSenha.Font = new Font("Segoe UI", 10F);
-            this.txtConfirmarNovaSenha.Location = new System.Drawing.Point(12, 110);
-            this.txtConfirmarNovaSenha.Name = "txtConfirmarNovaSenha";
-            this.txtConfirmarNovaSenha.Size = new System.Drawing.Size(260, 25);
-            this.txtConfirmarNovaSenha.TabIndex = 2;
-            this.txtConfirmarNovaSenha.Enter += new EventHandler(this.RemoverTextoPlaceholder);
-            this.txtConfirmarNovaSenha.Leave += new EventHandler(this.DefinirTextoPlaceholder);
-
+            // 
+            txtConfirmarNovaSenha.Font = new Font("Segoe UI", 10F);
+            txtConfirmarNovaSenha.Location = new Point(60, 250);
+            txtConfirmarNovaSenha.Name = "txtConfirmarNovaSenha";
+            txtConfirmarNovaSenha.Size = new Size(350, 25);
+            txtConfirmarNovaSenha.TabIndex = 2;
+            txtConfirmarNovaSenha.Enter += RemoverTextoPlaceholder;
+            txtConfirmarNovaSenha.Leave += DefinirTextoPlaceholder;
+            // 
             // btnAlterarSenha
-            this.btnAlterarSenha.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.btnAlterarSenha.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-            this.btnAlterarSenha.ForeColor = System.Drawing.Color.White;
-            this.btnAlterarSenha.Location = new System.Drawing.Point(12, 150);
-            this.btnAlterarSenha.Name = "btnAlterarSenha";
-            this.btnAlterarSenha.Size = new System.Drawing.Size(260, 30);
-            this.btnAlterarSenha.TabIndex = 3;
-            this.btnAlterarSenha.Text = "Alterar Senha";
-            this.btnAlterarSenha.UseVisualStyleBackColor = false;
-            this.btnAlterarSenha.Click += new EventHandler(this.btnAlterarSenha_Click);
-
+            // 
+            btnAlterarSenha.BackColor = Color.LightSkyBlue;
+            btnAlterarSenha.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnAlterarSenha.ForeColor = Color.White;
+            btnAlterarSenha.Location = new Point(60, 300);
+            btnAlterarSenha.Name = "btnAlterarSenha";
+            btnAlterarSenha.Size = new Size(350, 40);
+            btnAlterarSenha.TabIndex = 3;
+            btnAlterarSenha.Text = "Alterar Senha";
+            btnAlterarSenha.UseVisualStyleBackColor = false;
+            btnAlterarSenha.Click += btnAlterarSenha_Click;
+            // 
             // lblErro
-            this.lblErro.AutoSize = true;
-            this.lblErro.Font = new Font("Segoe UI", 9F);
-            this.lblErro.ForeColor = System.Drawing.Color.Red;
-            this.lblErro.Location = new System.Drawing.Point(12, 190);
-            this.lblErro.Name = "lblErro";
-            this.lblErro.Size = new System.Drawing.Size(0, 15);
-            this.lblErro.TabIndex = 4;
-
+            // 
+            lblErro.AutoSize = true;
+            lblErro.Font = new Font("Segoe UI", 9F);
+            lblErro.ForeColor = Color.Red;
+            lblErro.Location = new Point(60, 350);
+            lblErro.Name = "lblErro";
+            lblErro.Size = new Size(0, 15);
+            lblErro.TabIndex = 4;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Arial Black", 20.25F, FontStyle.Bold | FontStyle.Italic);
+            lblTitulo.Location = new Point(110, 52);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(272, 38);
+            lblTitulo.TabIndex = 5;
+            lblTitulo.Text = "ALTERAR SENHA";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(60, 232);
+            label3.Name = "label3";
+            label3.Size = new Size(134, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Confirmar Nova Senha";
+            label3.Click += label3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(60, 182);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 10;
+            label1.Text = "Nova Senha";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(60, 132);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Senha Atual";
+            // 
             // FormChangePassword
-            this.ClientSize = new System.Drawing.Size(284, 221);
-            this.Controls.Add(this.lblErro);
-            this.Controls.Add(this.btnAlterarSenha);
-            this.Controls.Add(this.txtConfirmarNovaSenha);
-            this.Controls.Add(this.txtNovaSenha);
-            this.Controls.Add(this.txtSenhaAtual);
-            this.Name = "FormChangePassword";
-            this.Text = "Alterar Senha";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            // 
+            ClientSize = new Size(470, 400);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(label3);
+            Controls.Add(lblTitulo);
+            Controls.Add(lblErro);
+            Controls.Add(btnAlterarSenha);
+            Controls.Add(txtConfirmarNovaSenha);
+            Controls.Add(txtNovaSenha);
+            Controls.Add(txtSenhaAtual);
+            Name = "FormChangePassword";
+            Text = "Alterar Senha";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         private TextBox txtSenhaAtual;
@@ -209,6 +267,14 @@ namespace LoginApp
         private TextBox txtConfirmarNovaSenha;
         private Button btnAlterarSenha;
         private Label lblErro;
+        private Label lblTitulo;
+
         private string senha;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
