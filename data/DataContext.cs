6397a16aca1d02ec1b165f5be8ac837985a17d23
Gabriel Fernandes <129302174+Se77ings@users.noreply.github.com>
2024-07-23 22:07:36 -0300

    Fixd

--- data/DataContext.cs
+++ data/DataContext.cs
@@ -29,21 +29,21 @@ public class DataContext : DbContext
     private void Seed(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categorias>().HasData(
-            new Categorias { ID = 1, descricao = "Alimentação", tipo = "Entrada" },
-            new Categorias { ID = 2, descricao = "Transporte", tipo = "Entrada" },
-            new Categorias { ID = 3, descricao = "Educação", tipo = "Entrada" },
-            new Categorias { ID = 4, descricao = "Transporte", tipo = "Entrada" },
-            new Categorias { ID = 5, descricao = "Educação", tipo = "Entrada" },
-            new Categorias { ID = 6, descricao = "Lazer", tipo = "Entrada" },
-            new Categorias { ID = 7, descricao = "Cuidados Pessoais", tipo = "Entrada" },
-            new Categorias { ID = 8, descricao = "Investimento", tipo = "Entrada" },
-            new Categorias { ID = 9, descricao = "Impostos", tipo = "Entrada" },
-            new Categorias { ID = 10, descricao = "Saúde", tipo = "Entrada" },
-            new Categorias { ID = 11, descricao = "Outras despesas", tipo = "Entrada" },
-            new Categorias { ID = 12, descricao = "Salário", tipo = "Saída" },
-            new Categorias { ID = 13, descricao = "Dividendos", tipo = "Saída" },
-            new Categorias { ID = 14, descricao = "13° Salário", tipo = "Saída" },
-            new Categorias { ID = 15, descricao = "Alugueis", tipo = "Saída" }
+            new Categorias { ID = 1, descricao = "Alimentação", tipo = "Saída" },
+            new Categorias { ID = 2, descricao = "Transporte", tipo = "Saída" },
+            new Categorias { ID = 3, descricao = "Educação", tipo = "Saída" },
+            new Categorias { ID = 4, descricao = "Transporte", tipo = "Saída" },
+            new Categorias { ID = 5, descricao = "Educação", tipo = "Saída" },
+            new Categorias { ID = 6, descricao = "Lazer", tipo = "Saída" },
+            new Categorias { ID = 7, descricao = "Cuidados Pessoais", tipo = "Saída" },
+            new Categorias { ID = 8, descricao = "Investimento", tipo = "Saída" },
+            new Categorias { ID = 9, descricao = "Impostos", tipo = "Saída" },
+            new Categorias { ID = 10, descricao = "Saúde", tipo = "Saída" },
+            new Categorias { ID = 11, descricao = "Outras despesas", tipo = "Saída" },
+            new Categorias { ID = 12, descricao = "Salário", tipo = "Entrada" },
+            new Categorias { ID = 13, descricao = "Dividendos", tipo = "Entrada" },
+            new Categorias { ID = 14, descricao = "13° Salário", tipo = "Entrada" },
+            new Categorias { ID = 15, descricao = "Alugueis", tipo = "Entrada" }
         );
     }
 }
